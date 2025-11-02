51f53a96d5cfbf20bbdf36ed14149b49cc982dbd
Allen Conlon <software@conlon.dev>
2025-11-02 08:42:13 -0500

    ci: add some debug logic to scripts

--- .github/hack/update.sh
+++ .github/hack/update.sh
@@ -7,23 +7,45 @@ declare -a FLAVOR=("upstream" "registry1")
 readarray resourceMap < <(yq -o=j -I=0 '.projects[]' .github/hack/resources.json)
 
 for flavor in "${FLAVOR[@]}"; do
+  echo "::debug::flavor='${flavor}'"
   for resource in "${resourceMap[@]}"; do
     path=$(echo "$resource" | yq e '.path' -)
+    echo "::debug::path='${path}'"
     url=$(echo "$resource" | yq e '.repo.url' -)
+    echo "::debug::url='${url}'"
     tag=$(echo "$resource" | yq e '.repo.tag' -)
+    echo "::debug::tag='${tag}'"
     file=$(echo "$resource" | yq e '.repo.path' -)
-    tempDir="$(mktemp -d -t temp.XXXXXX)"
+    echo "::debug::file='${file}'"
+    match=$(echo "$resource" | yq e '.file.match')
+    echo "::debug::match='${match}'"
 
+    tempDir="$(mktemp -d -t temp.XXXXXX)"
+    echo "::debug::tempDir='${tempDir}'"
+
+    rm -rf $flavor/$path
     mkdir -p $flavor/$path
 
     if [ `echo "$resource" | yq e '.file.extract'` = "true" ]; then
+      echo "::debug::extracting from release tar ball"
       gh release download --repo $url --pattern $file --dir $tempDir --clobber $tag
 
-      tar -xf $tempDir/$file -C $flavor/$path --overwrite
+      if [ $match = "null" ] ; then
+        tar -xf $tempDir/$file -C $flavor/$path --overwrite
+      else
+        tar -xf $tempDir/$file -C $tempDir --overwrite
+        cp $tempDir/$match $flavor/$path
+      fi
     elif [ `echo "$resource" | yq e '.repo.release'` = "true" ]; then
-      gh release download --repo $url --pattern $file --dir $flavor/$path --clobber $tag
+      echo "::debug::pulling from release"
+      if [ $match = "null" ] ; then
+        gh release download --repo $url --pattern $file --dir $flavor/$path --clobber $tag
+      else
+        gh release download --repo $url --pattern $file --dir $tempDir --clobber $tag
+        cp $tempDir/$match $flavor/$path
+      fi
     else
-      match=$(echo "$resource" | yq e '.file.match')
+      echo "::debug::pulling from git repo"
       gh repo clone $url $tempDir
       git -C $tempDir checkout tags/$tag
 
@@ -37,5 +59,6 @@ for flavor in "${FLAVOR[@]}"; do
     fi
 
     rm -rf $tempDir
+    git restore $flavor/$path/kustomization.yaml || true
   done
 done
--- .github/hack/zarf.sh
+++ .github/hack/zarf.sh
@@ -4,19 +4,27 @@ set -u
 set -o pipefail
 
 declare -a FLAVOR=("upstream" "registry1")
+declare -a COMPONENTS=("core" "image-controller" "source-watcher")
 
 for flavor in "${FLAVOR[@]}"; do
   echo "::debug::flavor='${flavor}'"
   rm -rf .direnv/$flavor
   mkdir -p .direnv/$flavor
   zarf dev find-images --flavor $flavor --skip-cosign . 2>/dev/null > .direnv/$flavor/out.yml
-  export yq_search=$(printf '.components[] | select(.name == "fluxcd-images" and .only.flavor == "%s") | path | .[1]' "$flavor")
-  echo "::debug::yq_search='${yq_search}'"
-  export yq_index=$(yq "$yq_search" zarf.yaml)
-  echo "::debug::yq_index='${yq_index}'"
-  export yq_images=$(printf '(select(fi == 1) | .components[] | select(.name == "fluxcd-manifests") | .images | ... head_comment="") as $img')
-  echo "::debug::yq_images='${yq_images}'"
-  export yq_update=$(printf '(select(fi == 0) | .components[%s].images = ($img | sort))' "$yq_index")
-  echo "::debug::yq_update='${yq_update}'"
-  yq ea -i "$yq_images | $yq_update" zarf.yaml .direnv/$flavor/out.yml
+
+  for component in "${COMPONENTS[@]}"; do
+    if [[ "$flavor" == "registry1" && "$component" == "source-watcher" ]]; then
+      continue
+    fi
+    export yq_component=$(printf '.components[] | select(.only.flavor == "%s" and .name == "fluxcd-%s") | path | .[1]' "$flavor" "$component")
+    echo "::debug::yq_component='${yq_component}'"
+    export yq_index=$(yq "$yq_component" zarf.yaml)
+    echo "::debug::yq_index='${yq_index}'"
+    export yq_images=$(printf '(select(fi == 1) | .components[] | select(.name == "fluxcd-%s") | .images | ... head_comment="") as $img' "$component")
+    echo "::debug::yq_images='${yq_images}'"
+    export yq_update=$(printf '(select(fi == 0) | .components[%s].images = ($img | sort))' "$yq_index")
+    echo "::debug::yq_update='${yq_update}'"
+    echo "::debug::debug='${yq_images} | ${yq_update}'"
+    yq ea -i "$yq_images | $yq_update" zarf.yaml .direnv/$flavor/out.yml
+  done
 done
